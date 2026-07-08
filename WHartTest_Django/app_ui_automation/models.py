613d94651b745f71dd46c38e630ab074e0e6436e
still-dream <940462056@qq.com>
2026-07-08 12:24:29 +0800

    feat: add AppUiScript model with .air zip upload support

--- WHartTest_Django/app_ui_automation/models.py
+++ WHartTest_Django/app_ui_automation/models.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 """APPUI 自动化数据模型"""
 
+import os
+
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
@@ -47,3 +50,58 @@ class AppUiModule(models.Model):
     def save(self, *args, **kwargs):
         self.clean()
         super().save(*args, **kwargs)
+
+
+def app_ui_script_path(instance, filename):
+    """脚本文件存储路径: app_ui_scripts/{project_id}/{script_id}/"""
+    return f"app_ui_scripts/{instance.project.id}/{instance.id}/{filename}"
+
+
+class AppUiScript(models.Model):
+    """APPUI Airtest 脚本"""
+    PLATFORM_CHOICES = [('android', _('Android')), ('ios', _('iOS'))]
+    STATUS_CHOICES = [
+        ('idle', _('空闲')), ('running', _('执行中')),
+        ('success', _('成功')), ('failed', _('失败')),
+    ]
+    LEVEL_CHOICES = [('P0', 'P0'), ('P1', 'P1'), ('P2', 'P2'), ('P3', 'P3')]
+
+    project = models.ForeignKey(Project, on_delete=models.CASCADE,
+        related_name='app_ui_scripts', verbose_name=_('所属项目'))
+    module = models.ForeignKey(AppUiModule, on_delete=models.PROTECT,
+        related_name='scripts', verbose_name=_('所属模块'))
+    name = models.CharField(_('脚本名称'), max_length=255)
+    description = models.TextField(_('脚本描述'), blank=True, null=True)
+    platform = models.CharField(_('目标平台'), max_length=10,
+        choices=PLATFORM_CHOICES, default='android')
+    script_file = models.FileField(_('Airtest脚本包'), upload_to=app_ui_script_path,
+        help_text=_('上传 .air 目录打包的 zip 文件'))
+    script_dir = models.CharField(_('脚本目录路径'), max_length=500, blank=True, default='')
+    script_entry = models.CharField(_('脚本入口文件'), max_length=255, blank=True, default='')
+    level = models.CharField(_('用例等级'), max_length=2, choices=LEVEL_CHOICES, default='P2')
+    status = models.CharField(_('最近状态'), max_length=10, choices=STATUS_CHOICES, default='idle')
+    creator = models.ForeignKey(User, on_delete=models.SET_NULL, null=True,
+        related_name='created_app_ui_scripts', verbose_name=_('创建人'))
+    created_at = models.DateTimeField(_('创建时间'), auto_now_add=True)
+    updated_at = models.DateTimeField(_('更新时间'), auto_now=True)
+
+    class Meta:
+        verbose_name = _('APPUI 脚本')
+        verbose_name_plural = _('APPUI 脚本')
+        ordering = ['-created_at']
+        db_table = 'app_ui_script'
+
+    def __str__(self):
+        return f"{self.project.name} - {self.name}"
+
+    def delete(self, *args, **kwargs):
+        if self.script_file:
+            path = self.script_file.path
+            if os.path.isfile(path):
+                os.remove(path)
+        if self.script_dir:
+            import shutil
+            full_dir = os.path.join(settings.MEDIA_ROOT, self.script_dir)
+            if os.path.isdir(full_dir):
+                shutil.rmtree(full_dir, ignore_errors=True)
+        super().delete(*args, **kwargs)
--- WHartTest_Django/app_ui_automation/tests.py
+++ WHartTest_Django/app_ui_automation/tests.py
@@ -1,7 +1,11 @@
+import io
+import zipfile
+
 from django.test import TestCase
 from django.contrib.auth.models import User
+from django.core.files.uploadedfile import SimpleUploadedFile
 from projects.models import Project
-from app_ui_automation.models import AppUiModule
+from app_ui_automation.models import AppUiModule, AppUiScript
 
 
 class AppUiModuleModelTest(TestCase):
@@ -34,3 +38,30 @@ class AppUiModuleModelTest(TestCase):
                 project=self.project, name=f'L{i+2}', parent=parent, creator=self.user
             )
         self.assertEqual(parent.level, 5)
+
+
+class AppUiScriptModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpass')
+        self.project = Project.objects.create(name='Test Project', creator=self.user)
+        self.module = AppUiModule.objects.create(
+            project=self.project, name='Test Module', creator=self.user
+        )
+
+    def _make_air_zip(self):
+        buf = io.BytesIO()
+        with zipfile.ZipFile(buf, 'w') as zf:
+            zf.writestr('test.air/test.py',
+                '# -*- encoding=utf8 -*-\nfrom airtest.core.api import *\nauto_setup(__file__)\n')
+            zf.writestr('test.air/tpl123.png', b'fake_png_data')
+        buf.seek(0)
+        return SimpleUploadedFile('test.air.zip', buf.read(), content_type='application/zip')
+
+    def test_create_script(self):
+        script = AppUiScript.objects.create(
+            project=self.project, module=self.module, name='Test Script',
+            platform='android', script_file=self._make_air_zip(), creator=self.user
+        )
+        self.assertEqual(script.name, 'Test Script')
+        self.assertEqual(script.status, 'idle')
+        self.assertTrue(script.script_file.name.startswith('app_ui_scripts/'))
